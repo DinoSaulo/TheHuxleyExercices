3fee1b1a0b825ea68dbc73a1b336b6de6cb4cc80
DinoSaulo <sab2@cin.ufpe.br>
2018-09-09 22:33:18 -0300

    adicionadas especificações

---
+++ Python/Beginner/1087-100 primeiros números naturais.java
@@ -0,0 +1,13 @@
+package solvedexercices;
+
+public class HuxleyCode {
+
+	public static void main(String[] args) {
+
+		// Link of exercice: https://www.thehuxley.com/problem/1087?locale=pt_BR
+
+		for(int i = 0; i <100; i++)
+			System.out.println(i);
+	}
+
+}
