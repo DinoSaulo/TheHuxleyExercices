ec7220015764644687b312fdcaf2f08f802e9f9e
DinoSaulo <sab2@cin.ufpe.br>
2018-09-25 14:54:54 -0600

    exercicio novo adicionado

---
+++ Java/Beginner/166 - Algarísmo da casa das unidades.java
@@ -0,0 +1,24 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class divisores {
+
+	private static Scanner in;
+	
+	// Link do Exercicio: https://www.thehuxley.com/problem/166/
+
+	public static void main(String[] args) {
+		
+		
+	Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt(); 
+		
+		int b = a % 10;
+	
+		System.out.println(b);
+		
+		}	
+	}
+
+		
