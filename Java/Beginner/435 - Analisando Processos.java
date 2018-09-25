ba69e823c7a463cd2d54f575494b49a16e952b87
DinoSaulo <sab2@cin.ufpe.br>
2018-09-25 14:59:06 -0600

    exercicio novo adicionado

---
+++ Java/Beginner/435 - Analisando Processos.java
@@ -0,0 +1,25 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class divisores {
+
+	private static Scanner in;
+    
+    // Link do Exercicio: https://www.thehuxley.com/problem/435?locale=pt_BR
+    
+	public static void main(String[] args) {
+		
+		
+		in = new Scanner(System.in);
+				
+		int h = in.nextInt();
+		int t = 8*60;
+		
+		System.out.println(t/h);
+		
+	}	
+	
+}
+
+		
+		
