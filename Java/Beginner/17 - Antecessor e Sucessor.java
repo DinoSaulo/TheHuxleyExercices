729e3295139d98e429d2db9a1155dbe03f893f2c
Saulo Alexandre de Barros <sab2@windows.cin.ufpe.br>
2018-09-26 09:51:20 -0300

    more exercices

---
+++ Java/Beginner/17 - Antecessor e Sucessor.java
@@ -0,0 +1,19 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class divisores {
+
+	public static void main(String[] args) {
+		
+		// link do Exercicio: https://www.thehuxley.com/problem/17?locale=pt_BR
+
+		Scanner in = new Scanner(System.in);
+		
+		int cav = in.nextInt();
+		System.out.print(cav-1+" ");
+		System.out.println(cav+1 );
+		
+		
+	}
+}
+	
